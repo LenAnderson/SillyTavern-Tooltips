9353403a17cc0629a5c68ff76bce9a1562ba959f
LenAnderson <Anderson.Len@outlook.com>
2024-06-20 15:54:30 -0400

    fix dialog overhaul

--- index.js
+++ index.js
@@ -43,6 +43,9 @@ const registerTooltips = ()=>{
                     tt.append(`${line}\n`);
                 }
             }
+            const layer = evt.target.closest('dialog, body').getBoundingClientRect();
+            tt.style.setProperty('--layer-left', `${layer.left}`);
+            tt.style.setProperty('--layer-top', `${layer.top}`);
             tt.style.setProperty('--left', `${evt.clientX + 10}`);
             tt.style.setProperty('--top', `${evt.clientY + 15}`);
             tt.style.setProperty('--right', `${window.innerWidth - evt.clientX}`);
@@ -50,7 +53,7 @@ const registerTooltips = ()=>{
             tt.classList.remove('sttt--flip-h');
             tt.classList.remove('sttt--flip-v');
             tt.classList.remove('sttt--active');
-            document.body.append(tt);
+            evt.target.closest('dialog, body').append(tt);
             const rect = tt.getBoundingClientRect();
             if (rect.right > window.innerWidth - 10) {
                 tt.classList.add('sttt--flip-h');
